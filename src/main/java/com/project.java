0907b52647953d098cf914adcfbe5295e0141617
gelingcong <3156251322@qq.com>
2022-07-03 17:03:44 +0800

    hot-fix commit

--- src/main/java/com/project.java
+++ src/main/java/com/project.java
@@ -5,5 +5,6 @@ public class project {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("如果你看见了就合并成功啦 我是hot-fix");
     }
 }
